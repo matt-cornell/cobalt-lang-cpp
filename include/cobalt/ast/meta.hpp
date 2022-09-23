ac88cdabe7121e9827cd537169b65db222b0b595
matt-cornell <mcornell400708@gmail.com>
2022-09-23 16:22:03 -0400

    Added support for pretty-printing of ASTs

--- include/cobalt/ast/meta.hpp
+++ include/cobalt/ast/meta.hpp
@@ -5,12 +5,16 @@ namespace cobalt::ast {
   struct llvm_ast : ast_base {
     std::string code;
     llvm_ast(location loc, std::string&& code) : ast_base(loc), code(std::move(code)) {}
-    private: typed_value codegen_impl(compile_context& ctx) const override;
+    private: 
+    typed_value codegen_impl(compile_context& ctx) const override;
+    void print_impl(llvm::raw_ostream& os, llvm::Twine prefix) const override;
   };
   struct asm_ast : ast_base {
     std::string code;
     asm_ast(location loc, std::string&& code) : ast_base(loc), code(std::move(code)) {}
-    private: typed_value codegen_impl(compile_context& ctx) const override;
+    private: 
+    typed_value codegen_impl(compile_context& ctx) const override;
+    void print_impl(llvm::raw_ostream& os, llvm::Twine prefix) const override;
   };
 }
 #endif

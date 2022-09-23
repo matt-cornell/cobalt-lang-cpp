b751f33fe646908796f28ccf73c447afeb7595f6
matt-cornell <mcornell400708@gmail.com>
2022-09-22 22:20:01 -0400

    Remove cobalt::ast::embed_ast

--- include/cobalt/ast/meta.hpp
+++ include/cobalt/ast/meta.hpp
@@ -2,11 +2,6 @@
 #define COBALT_AST_META_HPP
 #include "ast.hpp"
 namespace cobalt::ast {
-  struct embed_ast : ast_base {
-    std::string code;
-    embed_ast(std::string&& code, location loc) : ast_base(loc), code(std::move(code)) {}
-    private: typed_value codegen_impl(compile_context& ctx) const override;
-  };
   struct llvm_ast : ast_base {
     std::string code;
     llvm_ast(std::string&& code, location loc) : ast_base(loc), code(std::move(code)) {}

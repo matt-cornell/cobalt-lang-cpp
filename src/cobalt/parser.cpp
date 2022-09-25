93c427d4951522d5a784023219b3fda01488000e
matt-cornell <mcornell400708@gmail.com>
2022-09-25 08:34:54 -0400

    Began work on parser

--- src/cobalt/parser.cpp
+++ src/cobalt/parser.cpp
@@ -1,8 +1,27 @@
 #include "cobalt/parser.hpp"
 #include "cobalt/ast.hpp"
 using namespace cobalt;
+std::vector<std::string_view> bin_ops[] {
+  {"=", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "<<=", ">>=", "^^="},
+  {"||"},
+  {"&&"},
+  {"==", "!="},
+  {"<", "<=", ">", ">="},
+  {"|"},
+  {"&"},
+  {"<<", ">>"},
+  {"+", "-"},
+  {"*", "/", "%"},
+  {"^^"}
+};
+std::string_view pre_ops[] = {"+", "-", "*", "!", "~", "++", "--"};
+std::string_view post_ops[] = {"?", "!"};
+AST parse_expr(span<token> code, flags_t flags) {
+  
+}
 AST cobalt::parse(span<token> code, flags_t flags) {
+  AST ast = AST::create<ast::block_ast, std::vector<AST>&&>({});
   (void)code;
   (void)flags;
-  return nullptr;
+  return ast;
 }

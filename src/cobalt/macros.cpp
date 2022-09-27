11a195848c001e7e08153291aba67c60ec953a02
matt-cornell <mcornell400708@gmail.com>
2022-09-26 21:22:16 -0400

    Added conditional macro

--- src/cobalt/macros.cpp
+++ src/cobalt/macros.cpp
@@ -131,6 +131,29 @@ macro_map cobalt::default_macros {
     if (idx == std::string::npos) return "0";
     return (code.substr(0, idx) != code.substr(idx + 1)) ? "1" : "0";
   })
+  DEF_PP(cond, {
+    auto idx1 = code.find(';');
+    if (idx1 == std::string::npos) {
+      onerror("invalid format for @cond: fields must be separated by semicolons", ERROR);
+      return "";
+    }
+    std::string_view cond = code.substr(0, idx1), if_true, if_false;
+    auto idx2 = code.find(';', idx1 + 1);
+    if (idx2 == std::string::npos) if_true = code.substr(idx1 + 1);
+    else {
+      if_true = code.substr(idx1 + 1, idx2 - idx1 - 1);
+      if_false = code.substr(idx2 + 1);
+    }
+    if (cond.empty()) return std::string(if_false);
+    auto idx = cond.find_first_not_of(" \t\n\r\f");
+    if (idx == std::string::npos) return std::string(if_false);
+    cond.remove_prefix(idx);
+    idx = cond.find_last_not_of(" \t\n\r\f");
+    if (idx == std::string::npos) return std::string(if_false); // this should be unreachable, but I'm not taking any chances
+    cond.remove_suffix(idx);
+    if (cond.find_first_not_of('0') == std::string::npos) return std::string(if_false);
+    else return std::string(if_true);
+  })
   DEF_PP(region, {(void)code; return "";})
   DEF_PP(endregion, {(void)code; return "";})
   DEF_PP(version, {(void)code; return "\"" COBALT_VERSION "\"";})

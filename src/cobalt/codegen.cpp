933b0de0efae41ff9e88a139dfadd6a58ecdfd20
Matthew Cornell <mcornell400708@gmail.com>
2022-11-06 16:27:19 -0500

    Added code generation for literal values

--- src/cobalt/codegen.cpp
+++ src/cobalt/codegen.cpp
@@ -4,6 +4,7 @@
 #include "cobalt/types.hpp"
 using namespace cobalt;
 // flow.hpp
+const static auto f16 = sstring::get("f16"), f32 = sstring::get("f32"), f64 = sstring::get("f64"), f128 = sstring::get("f128"), isize = sstring::get("isize"), usize = sstring::get("usize");
 typed_value cobalt::ast::top_level_ast::codegen(compile_context& ctx) const {
   for (auto const& ast : insts) ast(ctx);
   return nullval;
@@ -33,10 +34,74 @@ typed_value cobalt::ast::subscr_ast::codegen(compile_context& ctx) const {(void)
 typed_value cobalt::ast::call_ast::codegen(compile_context& ctx) const {(void)ctx; return nullval;}
 typed_value cobalt::ast::fndef_ast::codegen(compile_context& ctx) const {(void)ctx; return nullval;}
 // literals.hpp
-typed_value cobalt::ast::integer_ast::codegen(compile_context& ctx) const {(void)ctx; return nullval;}
-typed_value cobalt::ast::float_ast::codegen(compile_context& ctx) const {(void)ctx; return nullval;}
-typed_value cobalt::ast::string_ast::codegen(compile_context& ctx) const {(void)ctx; return nullval;}
-typed_value cobalt::ast::char_ast::codegen(compile_context& ctx) const {(void)ctx; return nullval;}
+typed_value cobalt::ast::integer_ast::codegen(compile_context& ctx) const {
+  if (suffix.empty()) return {llvm::Constant::getIntegerValue(llvm::Type::getInt64Ty(*ctx.context), val), types::integer::get(64)};
+  switch (suffix.front()) {
+    case 'i':
+      if (suffix == isize) return {llvm::Constant::getIntegerValue(llvm::Type::getIntNTy(*ctx.context, sizeof(void*) * 8), val), types::integer::get(sizeof(void*) * 8, false)};
+      else if (suffix.find_first_not_of("012456789", 1) == std::string::npos) {
+        unsigned width = 0;
+        for (char c : suffix.substr(1)) {
+          width *= 10;
+          width += c - '0';
+        }
+        return {llvm::ConstantInt::get(llvm::Type::getIntNTy(*ctx.context, width), val), types::integer::get(width, false)};
+      }
+      else goto UNKNOWN;
+    case 'u':
+      if (suffix == usize) return {llvm::Constant::getIntegerValue(llvm::Type::getIntNTy(*ctx.context, sizeof(void*) * 8), val), types::integer::get(sizeof(void*) * 8, true)};
+      else if (suffix.find_first_not_of("012456789", 1) == std::string::npos) {
+        unsigned width = 0;
+        for (char c : suffix.substr(1)) {
+          width *= 10;
+          width += c - '0';
+        }
+        return {llvm::ConstantInt::get(llvm::Type::getIntNTy(*ctx.context, width), val), types::integer::get(width, true)};
+      }
+      else goto UNKNOWN;
+    default: UNKNOWN:
+      ctx.flags.onerror(loc, (llvm::Twine("unknown suffix '") + suffix + "' on integer literal").str(), ERROR);
+      return nullval;
+  }
+}
+typed_value cobalt::ast::float_ast::codegen(compile_context& ctx) const {
+  if (suffix.empty()) return {llvm::ConstantFP::get(llvm::Type::getDoubleTy(*ctx.context), val), types::float64::get()};
+  if (suffix.front() == 'f' && suffix.find_first_not_of("0123456789") == std::string::npos) {
+    if (suffix == f16) return {llvm::ConstantFP::get(llvm::Type::getHalfTy(*ctx.context), val), types::float16::get()};
+    else if (suffix == f32) return {llvm::ConstantFP::get(llvm::Type::getFloatTy(*ctx.context), val), types::float32::get()};
+    else if (suffix == f64) return {llvm::ConstantFP::get(llvm::Type::getDoubleTy(*ctx.context), val), types::float64::get()};
+    else if (suffix == f128) return {llvm::ConstantFP::get(llvm::Type::getFP128Ty(*ctx.context), val), types::float128::get()};
+    else {
+      ctx.flags.onerror(loc, "invalid floating point suffix, only f16, f32, f64, and f128 are allowed", ERROR);
+      return nullval;
+    }
+  }
+  else {
+    ctx.flags.onerror(loc, (llvm::Twine("unknown suffix '") + suffix + "' on integer literal").str(), ERROR);
+    return nullval;
+  }
+}
+typed_value cobalt::ast::string_ast::codegen(compile_context& ctx) const {
+  std::vector<llvm::Constant*> elems;
+  elems.reserve(val.size() + 1);
+  auto i8_ty = llvm::Type::getInt8Ty(*ctx.context);
+  for (char c : val) elems.push_back(llvm::ConstantInt::get(i8_ty, c));
+  elems.push_back(llvm::ConstantInt::get(i8_ty, 0));
+  return {llvm::ConstantArray::get(llvm::ArrayType::get(i8_ty, 0), elems), types::pointer::get(types::integer::get(8))};
+}
+typed_value cobalt::ast::char_ast::codegen(compile_context& ctx) const {
+  if (!suffix.empty()) {
+    ctx.flags.onerror(loc, "character literal cannot have a suffix", ERROR);
+    return nullval;
+  }
+  llvm::APInt out(32, 0, true);
+  std::size_t shift = 0;
+  for (char c : val) {
+    out |= (unsigned char)c << shift;
+    shift += 4;
+  }
+  return {llvm::ConstantInt::get(llvm::Type::getInt32Ty(*ctx.context), out), types::integer::get(32)};
+}
 // scope.hpp
 typed_value cobalt::ast::module_ast::codegen(compile_context& ctx) const {(void)ctx; return nullval;}
 typed_value cobalt::ast::import_ast::codegen(compile_context& ctx) const {(void)ctx; return nullval;}

ff4592a6298e56e1a96d99196e7f8f88ca58da6c
Matthew Cornell <mcornell400708@gmail.com>
2022-11-27 10:24:51 -0500

    Fixed parsing of arrays

--- src/cobalt/ast-type.cpp
+++ src/cobalt/ast-type.cpp
@@ -141,11 +141,10 @@ static type_ptr parse_type(sstring str) {
       }
       if (depth) return nullptr;
       std::size_t len = 0;
-      llvm::outs() << *it << *(it + 1) << *(it + 2) << '\n';
-      for (it += 2; it != &str.back() - 1; ++it) {
-        if (*it < '0' || *it > '9') return nullptr;
+      for (auto it2 = it + 2; it2 != &str.back() - 1; ++it2) {
+        if (*it2 < '0' || *it2 > '9') return nullptr;
         len *= 10;
-        len += *it - '0';
+        len += *it2 - '0';
       }
       return types::array::get(parse_type(sstring::get(str.substr(0, it - str.data() + 1))), len);
     };
